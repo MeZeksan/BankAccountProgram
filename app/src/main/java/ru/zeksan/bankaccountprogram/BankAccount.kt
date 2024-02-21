c6d2a8d4280156644f7d5196931004444f1b95f2
MeZeksan <bulzak74848@gmail.com>
2024-02-21 18:26:18 +0500

    Добавил реализацию функций. Двух людей и их счет и т.п.

--- app/src/main/java/ru/zeksan/bankaccountprogram/BankAccount.kt
+++ app/src/main/java/ru/zeksan/bankaccountprogram/BankAccount.kt
@@ -2,18 +2,30 @@ package ru.zeksan.bankaccountprogram
 
 class BankAccount(
     var accountHolder:String,
-    var balance: Double
+    private var balance: Double
 ) {
     private val transactionHistory = mutableListOf<String>()
 
     fun deposit(amount:Double){
-
+        balance +=amount
+        transactionHistory.add("$accountHolder deposited $$amount")
     }
     fun withdraw(amount: Double){
-
+        if(amount<= balance) {
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        }
+        else println("You don't have enough money to withdraw $$amount ")
     }
     fun displayTransactionHistory(){
+        println("Transaction history for $accountHolder")
+        for(transaction in transactionHistory){
+            println(transaction)
+        }
 
+    }
+    fun acctBalance(){
+        println(balance)
     }
 
 }
--- app/src/main/java/ru/zeksan/bankaccountprogram/BankMain.kt
+++ app/src/main/java/ru/zeksan/bankaccountprogram/BankMain.kt
@@ -1,7 +1,24 @@
 package ru.zeksan.bankaccountprogram
 
 fun main() {
-    val meZeksanBankAccount = BankAccount("MeZeksan",69.420)
+    val meZeksanBankAccount = BankAccount("MeZeksan", 69.420)
+    meZeksanBankAccount.deposit(200.0)
+    meZeksanBankAccount.displayTransactionHistory()
 
-    println(meZeksanBankAccount.accountHolder)
+    meZeksanBankAccount.withdraw(300.0)
+    meZeksanBankAccount.deposit(3000.0)
+
+    meZeksanBankAccount.withdraw(2690.60)
+    meZeksanBankAccount.displayTransactionHistory()
+    print("MeZeksan's balance is ")
+    meZeksanBankAccount.acctBalance()
+
+
+    val sarahBankAccount = BankAccount("Sarah", 0.0)
+    sarahBankAccount.deposit(100.0)
+    sarahBankAccount.withdraw(10.0)
+    sarahBankAccount.deposit(300.0)
+    sarahBankAccount.displayTransactionHistory()
+    print("Sarah's balance is ")
+    sarahBankAccount.acctBalance()
 }
