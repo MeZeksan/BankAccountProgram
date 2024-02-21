7f82955b17f621ac9bbf32912bc8188a8a2ac6a5
MeZeksan <bulzak74848@gmail.com>
2024-02-21 18:09:58 +0500

    Initial commit

---
+++ app/src/main/java/ru/zeksan/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,19 @@
+package ru.zeksan.bankaccountprogram
+
+class BankAccount(
+    var accountHolder:String,
+    var balance: Double
+) {
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount:Double){
+
+    }
+    fun withdraw(amount: Double){
+
+    }
+    fun displayTransactionHistory(){
+
+    }
+
+}
---
+++ app/src/main/java/ru/zeksan/bankaccountprogram/BankMain.kt
@@ -0,0 +1,7 @@
+package ru.zeksan.bankaccountprogram
+
+fun main() {
+    val meZeksanBankAccount = BankAccount("MeZeksan",69.420)
+
+    println(meZeksanBankAccount.accountHolder)
+}
